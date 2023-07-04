02f2d36060ab82c391b08cc144ee280056a401d6
EC2 Default User <ec2-user@ip-10-0-130-250.eu-west-2.compute.internal>
2023-07-04 12:07:16 +0000

    added a better button

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import logo from './logo.svg';
+import { Icon, Menu, Dropdown } from 'semantic-ui-react';
+
 import './App.css';
 function MyButton() {
   return (
@@ -8,14 +10,30 @@ function MyButton() {
 function App() {
   return (
     <div className="App">
+     <Menu fixed='top' color='blue' inverted>
+            <Menu.Menu>
+              <Menu.Item header href='/'><Icon name='globe' />Andy's Travel Deals</Menu.Item>
+            </Menu.Menu>
+            <Menu.Menu position='right'>
+              <Dropdown item simple text='User Name'>
+                <Dropdown.Menu>
+                  <Dropdown.Item onClick={() => alert('Log-out')}><Icon name='power off' />Log Out</Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+            </Menu.Menu>
+          </Menu>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
          
-      <h1>Welcome to my app</h1>
-      <MyButton />
+      <h1>Welcome to my rubbish app</h1>
+      <li>
+      The spinny thing above appearss to be in the App-header class blah
+        <MyButton />      
+      
+      </li>
+      
     
         <p>
-          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
